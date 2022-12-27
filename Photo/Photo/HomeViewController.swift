3d3db5f714b34fac07bc6f3610c5088b0e24fa8b
Kamal Harariya <harariyakamal@gmail.com>
2022-12-26 20:38:19 -0800

    Initial setup! (there is a warning in the console. Will check it out or you guys try to fix the issue).

---
+++ Photo/Photo/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  Photo
+//
+//  Created by Kamal Harariya on 12/26/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
